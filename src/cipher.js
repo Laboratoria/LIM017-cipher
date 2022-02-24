991c952cfb1026aedbf401e7fabc64088e6a35f1
arelia10 <arelia101195@gmail.com>
2022-02-24 11:19:42 -0500

    indexjs

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,31 @@
 const cipher = {
-  // ...
-};
-
-export default cipher;
+    encode: function  (offset , string) {
+      if(string===undefined){ throw new TypeError("el valor ingresado no es una cadena de texto")}
+      let messg = "";
+      for (let i = 0; i < string.length; i++) {
+        if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+          let ascciPosition = string.charCodeAt(i);
+          let offsetPosition = ((ascciPosition - 65 + parseInt(offset)) % 26) + 65;
+          let palabra = String.fromCharCode(offsetPosition);
+          messg = messg + palabra;
+        }
+      }
+      console.log(messg);
+      return messg;
+    },
+    decode: function (offset, string) {
+      if(string===undefined){ throw new TypeError("el valor ingresado no es una cadena de texto")}
+      let messg = "";
+      for (let i = 0; i < string.length; i++) {
+        if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+          let ascciPosition = string.charCodeAt(i);
+          let offsetPosition = ((ascciPosition + 65 - parseInt(offset)) % 26) + 65;
+          let palabra = String.fromCharCode(offsetPosition);
+          messg = messg + palabra;
+        }
+      }
+      console.log(messg);
+      return messg;
+    }
+  };
+  export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-import cipher from './cipher.js';
+/*import { inputRequiresLabel} from  'html.hint';*/
+import cipher from "./cipher.js";
+//declaro el boton  inicio//
+const descripcion_button=  document.getElementById("btn1");
+const return_button=  document.getElementById("btn4");//llama al boton del html//
+// le damos funcionabilidad al boton inicio para que oculte la primera ventana y pase a la segunda ventana//
+descripcion_button.addEventListener("click",()=>{ // Al botón se le adicióna el click y lo que sucede cuando éste evento sucede.//
+    const window1=document.querySelector(".window1");
+    const window2=document.querySelector(".window2");
+    window1.style.display="none";
+    window2.style.display="block";
+});
+// le damos funcionabilidad al boton para que regrese a la ventana inicial//
+return_button.addEventListener("click",()=>{// Al botón se le adicióna el click y lo que sucede cuando éste evento sucede.//
+    const window2=document.querySelector(".window2");
+    const window1=document.querySelector(".window1");
+    window2.style.display="none";
+    window1.style.display="block";
+});
 
-console.log(cipher);
