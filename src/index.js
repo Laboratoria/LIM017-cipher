03e721f4f44a5a8a28ce23162b1fd14074c28da3
arelia10 <arelia101195@gmail.com>
2022-02-24 13:14:17 -0500

    estoy cambiando el index js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 /*import { inputRequiresLabel} from  'html.hint';*/
 import cipher from "./cipher.js";
 //declaro el boton  inicio//
-const descripcion_button=  document.getElementById("btn1");
-const return_button=  document.getElementById("btn4");//llama al boton del html//
+const descripcion_button=  document.getElementById("btn1");//llama al boton del html//
 // le damos funcionabilidad al boton inicio para que oculte la primera ventana y pase a la segunda ventana//
 descripcion_button.addEventListener("click",()=>{ // Al botón se le adicióna el click y lo que sucede cuando éste evento sucede.//
     const window1=document.querySelector(".window1");
@@ -10,6 +9,7 @@ descripcion_button.addEventListener("click",()=>{ // Al botón se le adicióna e
     window1.style.display="none";
     window2.style.display="block";
 });
+const return_button=  document.getElementById("btn4");//llama al boton del html//
 // le damos funcionabilidad al boton para que regrese a la ventana inicial//
 return_button.addEventListener("click",()=>{// Al botón se le adicióna el click y lo que sucede cuando éste evento sucede.//
     const window2=document.querySelector(".window2");
