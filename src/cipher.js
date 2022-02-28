0edb0ffa205b210f84169c1e6b5a0ab0f6b0156f
arelia10 <arelia101195@gmail.com>
2022-02-28 15:19:19 -0500

    termine con index js y cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,31 +1,57 @@
 const cipher = {
-    encode: function  (offset , string) {
-      if(string===undefined){ throw new TypeError("el valor ingresado no es una cadena de texto")}
-      let messg = "";
-      for (let i = 0; i < string.length; i++) {
-        if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
-          let ascciPosition = string.charCodeAt(i);
-          let offsetPosition = ((ascciPosition - 65 + parseInt(offset)) % 26) + 65;
-          let palabra = String.fromCharCode(offsetPosition);
-          messg = messg + palabra;
-        }
-      }
-      console.log(messg);
-      return messg;
-    },
-    decode: function (offset, string) {
-      if(string===undefined){ throw new TypeError("el valor ingresado no es una cadena de texto")}
-      let messg = "";
-      for (let i = 0; i < string.length; i++) {
-        if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
-          let ascciPosition = string.charCodeAt(i);
-          let offsetPosition = ((ascciPosition + 65 - parseInt(offset)) % 26) + 65;
-          let palabra = String.fromCharCode(offsetPosition);
-          messg = messg + palabra;
-        }
-      }
-      console.log(messg);
-      return messg;
+  encode: function (offset, string) {
+    if (string === undefined) {
+      throw new TypeError("el valor ingresado no es una cadena de texto");
     }
-  };
-  export default cipher;
+    if (offset == undefined || string == [] || offset == "0" || offset == "0") {
+      throw new TypeError("bad arguments");
+    }
+    let messg = "";
+    for (let i = 0; i < string.length; i++) {
+      if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+        let ascciPosition = string.charCodeAt(i);
+        let offsetPosition =
+          ((ascciPosition - 65 + parseInt(offset)) % 26) + 65;
+        let palabra = String.fromCharCode(offsetPosition);
+        messg = messg + palabra;
+      } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+        let ascciPosition = string.charCodeAt(i);
+        let offsetPosition =
+          ((ascciPosition - 97 + parseInt(offset)) % 26) + 97;
+        let palabra = String.fromCharCode(offsetPosition);
+        messg = messg + palabra;
+      } else {
+        messg += string[i];
+      }
+    }
+    return messg;
+  },
+  decode: function (offset, string) {
+    if (string === undefined) {
+      throw new TypeError("el valor ingresado no es una cadena de texto");
+    }
+    if (offset == undefined || string == [] || offset == "0" || offset == "0") {
+      throw new TypeError("bad arguments");
+    }
+    let messg = "";
+    for (let i = 0; i < string.length; i++) {
+      if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+        let ascciPosition = string.charCodeAt(i);
+        let offsetPosition =
+          ((ascciPosition + 65 - parseInt(offset)) % 26) + 65;
+        let palabra = String.fromCharCode(offsetPosition);
+        messg = messg + palabra;
+      } else if (string.charCodeAt(i) >= 85 && string.charCodeAt(i) <= 122) {
+        let ascciPosition = string.charCodeAt(i);
+        let offsetPosition =
+          ((ascciPosition + 85 - parseInt(offset)) % 26) + 97;
+        let palabra = String.fromCharCode(offsetPosition);
+        messg = messg + palabra;
+      } else {
+        messg += string[i];
+      }
+    }
+    return messg;
+  },
+};
+export default cipher;
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -39,9 +39,9 @@ describe('cipher', () => {
     // Se quiser adicionar testes para letras minúsculas, descomente o teste
     // abaixo.
     //
-    // it('should return "hijklmnopqrstuvwxyzabcdefg" for "abcdefghijklmnopqrstuvwxyz" with offset 33', () => {
-    //   expect(cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz')).toBe('hijklmnopqrstuvwxyzabcdefg');
-    // });
+    it('should return "hijklmnopqrstuvwxyzabcdefg" for "abcdefghijklmnopqrstuvwxyz" with offset 33', () => {
+       expect(cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz')).toBe('hijklmnopqrstuvwxyzabcdefg');
+    });
 
     // Hacker edition
     //
@@ -86,9 +86,9 @@ describe('cipher', () => {
     // Se quiser adicionar testes para letras minúsculas, descomente o teste
     // abaixo.
     //
-    // it('should return "abcdefghijklmnopqrstuvwxyz" for "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
-    //   expect(cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg')).toBe('abcdefghijklmnopqrstuvwxyz');
-    // });
+     it('should return "abcdefghijklmnopqrstuvwxyz" for "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
+       expect(cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg')).toBe('abcdefghijklmnopqrstuvwxyz');
+     });
 
     // Hacker edition
     //
